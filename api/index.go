cd92adbc5fcd4ba62eb4ed9b86c3d26defaf10a1
BrunoFG <b.frontiniganem@bssweb.it>
2023-09-20 16:18:02 +0200

    fix go version for vercel

--- api/index.go
+++ api/index.go
@@ -12,9 +12,8 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-// Handler is the main entry point of the application. Think of it like the main() method
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// This is needed to set the proper request path in `*fiber.Ctx`
+
 	r.RequestURI = r.URL.String()
 
 	handler().ServeHTTP(w, r)
