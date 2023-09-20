a320f7c08336b4583a26c329e0ccf41b610d4d9f
BrunoFG <b.frontiniganem@bssweb.it>
2023-09-20 17:25:32 +0200

    fixing connection to database 2

--- database/database.go
+++ database/database.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,15 +19,14 @@ type Dbinstance struct {
 var DB Dbinstance
 
 func ConnectDb() {
-	p := config.Config("POSTGRES_PORT")
-	port, err := strconv.ParseUint(p, 10, 32)
-	if err != nil {
-		fmt.Println("Error parsing str to int")
-	}
+	// p := config.Config("POSTGRES_PORT")
+	// port, err := strconv.ParseUint(p, 10, 32)
+	// if err != nil {
+	// 	fmt.Println("Error parsing str to int")
+	// }
 
 	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/Rome", config.Config("POSTGRES_HOST"), config.Config("POSTGRES_USER"), config.Config("POSTGRES_PASSWORD"), config.Config("POSTGRES_DATABASE"), port)
 	dsn := config.Config("POSTGRES_URL")
-	
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
