42883518926687d9c6c43b5b199ee64bfe9e406f
BrunoFG <b.frontiniganem@bssweb.it>
2023-09-21 13:03:16 +0200

    first vercel commit

---
+++ api/index.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"Sviluppo/go/go-fiber/database"
+	"Sviluppo/go/go-fiber/router"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	r.RequestURI = r.URL.String()
+
+	handler().ServeHTTP(w, r)
+}
+
+// building the fiber application
+func handler() http.HandlerFunc {
+	database.ConnectDb()
+
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Static("/", "./public")
+
+	router.SetUpRoutes(app)
+	app.Use(cors.New())
+
+	// 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+
+	return adaptor.FiberApp(app)
+
+}
