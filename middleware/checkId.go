353a98efe61ee80d60134c5f09f228ddbb2c99a7
BrunoFG <b.frontiniganem@bssweb.it>
2023-09-22 17:55:07 +0200

    check id implemented

---
+++ middleware/checkId.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func CheckId() fiber.Handler {
+
+	return func(c *fiber.Ctx) error {
+
+		tokenString := extractToken(c)
+
+		id := c.Params("id")
+
+		token, err := jwt.Parse(tokenString, jwtKeyFunc)
+
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Failing chicking the role", "data": err.Error()})
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		if claims["user_role"] == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Ask for a user role to Alessandro", "data": nil})
+		}
+
+		if claims["user_id"] != id {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "You can only edit your profile", "data": nil})
+		}
+		return c.Next()
+	}
+}
